d82450f4a9dfdd10e4b152bbda055c106a9ab86f
unknown <winero2004@yahoo.ca>
2024-07-04 00:25:33 +0100

    My Final Works with Frontend Assessment

---
+++ src/components/pages/NotFound404.jsx
@@ -0,0 +1,12 @@
+function NotFound404(){
+
+    return (
+       
+        <div className="container">
+         <h2>Not Found</h2>
+         <p>This page does not contain content you search for.</p>
+        </div>
+    )
+}
+
+export default NotFound404;
